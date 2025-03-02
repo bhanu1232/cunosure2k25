ca96a5e8c9526652f8cf376708a23f9d36ec07ea
bhanu prakash chowdam <jcrseduvalley@gmail.com>
2025-03-02 11:10:33 +0530

    error

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,22 @@
 import React from "react";
 import type { Metadata } from "next";
-// import { Inter } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 
-// const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "cynosure",
-  description: "cynosure tech fest of sv university",
+  title: "Cynosure 2024",
+  description: "Technical Fest",
+  icons: {
+    icon: "/favicon.ico", // Make sure this file exists in the public folder
+  },
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
