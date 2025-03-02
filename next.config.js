e48fb1edbae37e270b77275dfc584e4d825a3b58
bhanu prakash chowdam <jcrseduvalley@gmail.com>
2025-03-02 11:30:27 +0530

    srk

--- next.config.js
+++ next.config.js
@@ -3,8 +3,12 @@ const nextConfig = {
   output: "export",
   images: {
     unoptimized: true,
+    domains: ["firebasestorage.googleapis.com"],
   },
   trailingSlash: true,
+  swcMinify: true,
+  reactStrictMode: true,
+  poweredByHeader: false,
 };
 
 module.exports = nextConfig;
